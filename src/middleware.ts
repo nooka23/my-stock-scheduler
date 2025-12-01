492ba40af671dce673d47cbf6397b262912d3cc1
StockMaster <kmh9525@gmail.com>
2025-12-01 14:02:32 +0900

    thirt commit3

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,19 +4,19 @@ import type { NextRequest } from 'next/server';
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
-  // req와 res를 둘 다 넣어줘야 쿠키가 갱신됩니다!
+  // 미들웨어가 쿠키를 관리하도록 설정
   const supabase = createMiddlewareClient({ req, res });
 
   const {
     data: { session },
   } = await supabase.auth.getSession();
 
-  // 로그인 안 함 + 메인('/') 접근 -> 로그인 페이지로
+  // 로그인 안 한 사람이 메인('/') 접근 시 -> 로그인 페이지로
   if (!session && req.nextUrl.pathname === '/') {
     return NextResponse.redirect(new URL('/login', req.url));
   }
 
-  // 로그인 함 + 로그인 페이지('/login') 접근 -> 메인으로
+  // 로그인 한 사람이 로그인 페이지('/login') 접근 시 -> 메인으로
   if (session && req.nextUrl.pathname === '/login') {
     return NextResponse.redirect(new URL('/', req.url));
   }
